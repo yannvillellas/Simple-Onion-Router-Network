503fe127bdb26ea8112f1136a057d6521e4494d2
yannvillellas <yvillellas@gmail.com>
2024-03-10 23:00:24 +0100

    GET routes 🛣️

--- src/onionRouters/simpleOnionRouter.ts
+++ src/onionRouters/simpleOnionRouter.ts
@@ -2,6 +2,10 @@ import bodyParser from "body-parser";
 import express from "express";
 import { BASE_ONION_ROUTER_PORT } from "../config";
 
+let lastReceivedEncryptedMessage: string | null = null;
+let lastReceivedDecryptedMessage: string | null = null;
+let lastMessageDestination: number | null = null;
+
 export async function simpleOnionRouter(nodeId: number) {
   const onionRouter = express();
   onionRouter.use(express.json());
@@ -9,6 +13,18 @@ export async function simpleOnionRouter(nodeId: number) {
 
   onionRouter.get("/status", (req, res) => {
     res.send('live');
+  });
+
+  onionRouter.get("/getLastReceivedEncryptedMessage", (req, res) => {
+    res.json({ result: lastReceivedEncryptedMessage });
+  });
+  
+  onionRouter.get("/getLastReceivedDecryptedMessage", (req, res) => {
+    res.json({ result: lastReceivedDecryptedMessage });
+  });
+
+  onionRouter.get("/getLastMessageDestination", (req, res) => {
+    res.json({ result: lastMessageDestination });
   });
 
   const server = onionRouter.listen(BASE_ONION_ROUTER_PORT + nodeId, () => {
--- src/users/user.ts
+++ src/users/user.ts
@@ -2,6 +2,9 @@ import bodyParser from "body-parser";
 import express from "express";
 import { BASE_USER_PORT } from "../config";
 
+let lastReceivedMessage: string | null = null;
+let lastSentMessage: string | null = null;
+
 export type SendMessageBody = {
   message: string;
   destinationUserId: number;
@@ -14,6 +17,14 @@ export async function user(userId: number) {
 
   _user.get("/status", (req, res) => {
     res.send("live");
+  });
+  
+  _user.get("/getLastReceivedMessage", (req, res) => {
+    res.json({ result: lastReceivedMessage });
+  });
+
+  _user.get("/getLastSentMessage", (req, res) => {
+    res.json({ result: lastSentMessage });
   });
 
   const server = _user.listen(BASE_USER_PORT + userId, () => {
